baf5534624eef5399dd1f9423e27fa91f48f8de7
Maximilian Brandl <kontakt@brandl-maximilian.de>
2016-12-26 21:04:48 +0100

    Host 2

--- public/sketch.js
+++ public/sketch.js
@@ -35,7 +35,7 @@ function setup() {
     // LOCAL NETWORK
     //TRANSFER.connectToServer("http://192.168.0.105:1337");
     // LOCAL MACHINE
-    TRANSFER.connectToServer("http://pongmultiplayer.herokuapp.com");
+    TRANSFER.connectToServer("https://pongmultiplayer.herokuapp.com/");
 
     // FRAMERATE
     smooth();
